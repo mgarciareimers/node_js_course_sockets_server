dd81852421a0c9313051a89319814e89a1fe46fc
Miguel García Reimers <miguelgarciareimers@Miguels-MacBook-Pro.local>
2020-11-10 14:51:13 +0100

    Socket installed.

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require('./config/config');
+
+const express = require('express');
+const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+
+// Enable public folder.
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+// io => backend communication.
+const io = socketIO(server);
+
+
+
+
+
+
+server.listen(process.env.PORT, () => console.log('Listening port:', process.env.PORT));
